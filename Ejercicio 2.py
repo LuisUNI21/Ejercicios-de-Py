8dee7600c811edc80a5811545964a74166851623
LuisUNI21 <lopezvadoluis789@gmail.com>
2025-04-24 22:08:23 -0600

    Ejercicio 2

--- Ejercicio 2.py
+++ Ejercicio 2.py
@@ -17,3 +17,12 @@ conteo_tomatometer = df_movies['tomatometer_status'].value_counts()
 
 print("\nConteo por clasificación del tomatometer:")
 print(conteo_tomatometer)
+
+# Gráfico de pastel (pie chart)
+
+plt.figure(figsize=(6, 6))
+conteo_tomatometer.plot.pie(autopct='%1.1f%%', startangle=90, colors=['#ff6347', '#90ee90', '#ffd700'])
+plt.title('Distribución de Clasificaciones Tomatometer')
+plt.ylabel('')  # Oculta la etiqueta del eje Y
+plt.tight_layout()
+plt.show()
